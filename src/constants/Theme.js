bcd0a8fdefee4dacd73311f80c7c5dccaeba597a
Dingras <feracosentino@gmail.com>
2024-03-31 23:56:47 -0300

    Fuente

---
+++ src/constants/Theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const Theme = extendTheme({
+    fonts: {
+        body: "Ubuntu Mono, monospace",
+        heading: "Ubuntu Mono, monospace",
+    },
+});
+
+export default Theme;
