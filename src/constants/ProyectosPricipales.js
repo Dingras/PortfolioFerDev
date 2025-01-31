f7a4106699c2decf50a3eaebd5e5ee40a31c5e3e
Fernando Cosentino <feracosentino@gmail.com>
2025-01-31 18:56:49 -0300

    Actualización de proyectos

--- src/constants/ProyectosPricipales.js
+++ src/constants/ProyectosPricipales.js
@@ -1,6 +1,28 @@
 const Proyectos_Principales = [
     {
         'id' : 1,
+        'titulo' : 'BrowserPY',
+        'descripcion' : "Navegador web.",
+        'url_img' : 'BrowserPy.webp',
+        'url_repo_front' : 'https://github.com/Dingras/BrowserPY',
+        'url_repo_back' : 'https://github.com/Dingras/BrowserPY',
+        'url_app' : '',
+        'tecnologias' : ['Python','QT']
+    }
+    ,
+    {
+        'id' : 2,
+        'titulo' : 'Web Site Hidráulica MP',
+        'descripcion' : "Sitio web de taller de hidráulica, herrería y tornería.",
+        'url_img' : 'HidraulicaMP.webp',
+        'url_repo_front' : 'https://github.com/Dingras/HidraulicaMP',
+        'url_repo_back' : '',
+        'url_app' : 'http://www.hidraulicamp.com.ar/',
+        'tecnologias' : ['React', 'Boostrap', 'Vercel']
+    }
+    ,
+    {
+        'id' : 3,
         'titulo' : 'Carrd APP',
         'descripcion' : "Aplicación tipo Kanban, realizada en colaboración como proyecto final de estudios.",
         'url_img' : 'carrd.webp',
@@ -8,38 +30,7 @@ const Proyectos_Principales = [
         'url_repo_back' : 'https://github.com/Dingras/API_carrds',
         'url_app' : '',
         'tecnologias' : ['.NET', 'React', 'MySQL']
-    },
-    {
-        'id' : 2,
-        'titulo' : 'Web Site Hidraulica MP',
-        'descripcion' : "Sitio web de taller de hidraulica, herreria y torneria.",
-        'url_img' : 'HidraulicaMP.webp',
-        'url_repo_front' : 'https://github.com/Dingras/HidraulicaMP',
-        'url_repo_back' : '',
-        'url_app' : 'http://www.hidraulicamp.com.ar/',
-        'tecnologias' : ['React', 'Boostrap', 'Vercel']
-    },
-    {
-        'id' : 3,
-        'titulo' : 'Agenda',
-        'descripcion' : "Pequeña agenda de usuarios, creada en PHP. Crea tu usuario y administra tu agenda de tareas.",
-        'url_img' : 'agenda.webp',
-        'url_repo_front' : 'https://github.com/Dingras/Agenda',
-        'url_repo_back' : '',
-        'url_app' : 'https://agendadingras.000webhostapp.com/',
-        'tecnologias' : ['PHP','HTML','MySQL']
     }
-    // ,
-    // {
-    //     'id' : 4,
-    //     'titulo' : 'Calculadora',
-    //     'descripcion' : "Una simple calculadora.",
-    //     'url_img' : 'calculadora.png',
-    //     'url_repo_front' : 'https://github.com/Dingras/CalculadoraHTML',
-    //     'url_repo_back' : '',
-    //     'url_app' : 'http://churritocalculador.somee.com/',
-    //     'tecnologias' : ['JavaScript', 'CSS', 'HTML']
-    // }
 ]
 
 export default Proyectos_Principales
